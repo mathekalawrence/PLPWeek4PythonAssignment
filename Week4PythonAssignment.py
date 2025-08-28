2b7fc30e4c7425c42ddea86abfd8d84a4df6cd77
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-28 17:23:16 +0300

    Add files via upload

--- Week4PythonAssignment.py
+++ Week4PythonAssignment.py
@@ -8,4 +8,13 @@ def process_file_with_error_handling():
     while True:
         #Prompting the user to enter the filename
         input_filename = input("Please enter the name of the input file with .txt extension: ")
-        
+        output_filename = "processed_" + os.path.basename(input_filename)
+
+        #Exception handling
+        try:
+            #Read the input file contents
+            with open(input_filename, 'r') as infile:
+                original_text = infile.read()
+
+            print(f"Confirmed!..Successfully read the file: {input.filename}")
+            
